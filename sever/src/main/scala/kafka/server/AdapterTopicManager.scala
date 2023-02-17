48ebfa1506d03fa01db859ed9a8f652738ef2f31
wangzhuang <wangzhuang>
2023-02-17 16:20:24 +0800

    从平衡流程补全

--- sever/src/main/scala/kafka/server/AdapterTopicManager.scala
+++ sever/src/main/scala/kafka/server/AdapterTopicManager.scala
@@ -1,12 +1,13 @@
 package kafka.server
 
+import com.tong.kafka.common.utils.Utils
 import com.tong.kafka.common.{Node, TopicPartition}
 
 /**
  * 管理topic—partition 和 代理服务器的分配关系
  * kafka消息发送时，会向leader节点发送消息，同时拉取消息如果不指定优选节点，也是从leader节点拉取消息
- * 该类决定了负载均衡
- */
+ * 该类决定了消息的负载负载均衡
+ * */
 trait AdapterTopicManager {
   def getLeaderNode(topic: TopicPartition): Node
 
@@ -16,9 +17,10 @@ trait AdapterTopicManager {
 class HashTopicManager(val nodes: List[Node]) extends AdapterTopicManager {
   private val sortNode = nodes.sortBy(n => n.id)
   private val total: Int = sortNode.length
+
   override def getLeaderNode(topic: TopicPartition): Node = {
 
-    val mod = topic.hashCode % total
+    val mod = Utils.abs(topic.hashCode) % total
     sortNode(mod)
   }
 
